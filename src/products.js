3f479f47eb60c35f9790a41b5121c60db6fe3c84
Nikhil Agrawal <89804069+nikhilagrawxl@users.noreply.github.com>
2023-06-02 18:16:17 +0530

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,24 @@
+import product1 from './assets/1.webp'
+import product2 from './assets/2.jpeg'
+import product3 from './assets/3.webp'
+
+export const PRODUCTS=[
+    {
+        id:1,
+        productName:"Flower",
+        price:0.01,
+        productImage:product1,
+    },
+    {
+        id:2,
+        productName:"Pot",
+        price:0.02,
+        productImage:product2,
+    },
+    {
+        id:3,
+        productName:"Painting",
+        price:0.03,
+        productImage:product3,
+    },
+];
